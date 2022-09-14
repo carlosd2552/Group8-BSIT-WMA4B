c2958f429d122e6d5db9539518728710ab54986f
Carlos De Almeida <111640891+carlosd2552@users.noreply.github.com>
2022-09-14 21:16:58 +0800

    Methods
    Methods

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Addition {
+public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the 1st numb:");
+    int x = sc.nextInt();
+    System.out.println("Enter the 2nd numb:");
+    int y = sc.nextInt();
+
+System.out.println("The total sum is" + " " + (x + y));
+
+
+}
+}
---
+++ Modulus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Modulus {
+    
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st numb:");
+        int x = sc.nextInt();
+        System.out.println("Enter the 2nd numb:");
+        int y = sc.nextInt();
+    
+    System.out.println("Your answer is" + " " + (x % y));
+    
+    
+    }
+    }
+
